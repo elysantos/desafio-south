ae5df6bda6257278b283f2491c3cdc3763daa89e
Ely Santos <ely.franklin@acad.ifma.edu.br>
2022-12-28 00:03:36 -0300

    fix(database): corrected host

--- compose.sh
+++ compose.sh
@@ -1,7 +1,6 @@
 docker compose down
 
 docker build -t mock-user-info .mock-user-info/.
-docker run -d -p 8099:8080 mock-user-info
-docker compose build zookeeper kafka pgsql-dev desafio-south init-kafka
-docker compose up -d --force-recreate zookeeper kafka pgsql-dev desafio-south init-kafka
+docker compose build zookeeper kafka pgsql-dev desafio-south init-kafka mock-user-info
+docker compose up -d --force-recreate zookeeper kafka pgsql-dev desafio-south init-kafka mock-user-info
 
