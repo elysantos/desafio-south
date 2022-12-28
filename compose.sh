2ca73ec74863a6c8122afaa2343f1746d685a7b9
Ely Santos <ely.franklin@acad.ifma.edu.br>
2022-12-28 12:00:30 -0300

    fix(docker): corrected pg volume folder

--- compose.sh
+++ compose.sh
@@ -1,5 +1,5 @@
 docker compose down
-
+mkdir .infra/data
 docker build -t mock-user-info .mock-user-info/.
 docker compose build zookeeper kafka pgsql-dev desafio-south init-kafka mock-user-info
 docker compose up -d --force-recreate zookeeper kafka pgsql-dev desafio-south init-kafka mock-user-info
